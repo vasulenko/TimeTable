eabfd610ad6b5c62f1a647f683c36d5b84a68137
Max <vasulenko.max@gmail.com>
2017-12-11 01:30:30 +0200

    holilya

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,23 @@
 'use strict';
 let currentState = Array(8).fill(Array(5));
 console.log(currentState.slice());
-let selectedGroup;
+let selectedGroup,
+  selectedPeople,
+  selectedRoom;
 
 const getInitState = (group = '') => {
   const groupName = group === '' ? selectedGroup : group;
   let xhr = new XMLHttpRequest();
   const url = 'http://localhost:8000/storage?group=' + groupName;
   xhr.open('GET', url, false);
+  xhr.send();
+  return JSON.parse(xhr.responseText);
+};
+
+const getStateByPeople = _ => {
+  let xhr = new XMLHttpRequest();
+  const url = 'http://localhost:8000/people';
+  xhr.open('POST', url, false);
   xhr.send();
   return JSON.parse(xhr.responseText);
 };
@@ -62,23 +72,38 @@ const saveState = _ => {
     let row = [...cell.children];
     let res = [];
     row.forEach((el, j) => {
-      if (el.children.length === 2) {
-        res[j] = el.children[0].value === el.children[1].value ? {
-          'top': el.children[0].value,
-          'bottom': el.children[0].value
-        } : {
-          'top': el.children[0].value,
-          'bottom': el.children[1].value
-        };
-      } else {
-        res[j] = el.children[0].value === '' ? {
-          'top': el.children[1].value,
-          'bottom': el.children[1].value
-        } : {
-          'top': el.children[0].value,
-          'bottom': el.children[0].value
-        };
-      }
+      let contentTop = el.children[0].value,
+        roomTop = el.children[1].value,
+        peopleTop = el.children[2].value,
+        contentBottom = el.children[3].value,
+        roomBottom = el.children[4].value,
+        peopleBottom = el.children[5].value;
+      res[j] = contentTop === contentBottom
+      && peopleTop === peopleBottom
+      && roomTop === roomBottom ? {
+        'top': {
+          context: contentTop,
+          room: roomTop,
+          people: peopleTop
+        },
+        'bottom': {
+          context: contentTop,
+          room: roomTop,
+          people: peopleTop
+        }
+      } : {
+        'top': {
+          context: contentTop,
+          room: roomTop,
+          people: peopleTop
+        },
+        'bottom': {
+          context: contentBottom,
+          room: roomBottom,
+          people: peopleBottom
+        }
+      };
+
     });
     currentState.push(res);
   });
@@ -101,7 +126,23 @@ const getAllGroup = _ => {
   return JSON.parse(xhr.responseText);
 };
 
-window.onload = () => {
+const getAllPeople = _ => {
+  let xhr = new XMLHttpRequest();
+  const url = 'http://localhost:8000/people';
+  xhr.open('GET', url, false);
+  xhr.send();
+  return JSON.parse(xhr.responseText);
+};
+
+const getAllRoom = _ => {
+  let xhr = new XMLHttpRequest();
+  const url = 'http://localhost:8000/room';
+  xhr.open('GET', url, false);
+  xhr.send();
+  return JSON.parse(xhr.responseText);
+};
+
+const initDropdown = _ => {
   let groupsName = getAllGroup();
   groupsName.forEach(el => {
     let option = document.createElement('option');
@@ -109,13 +150,55 @@ window.onload = () => {
     option.innerText = el;
     $('#group_select').get(0).appendChild(option);
   });
+
+  let roomsName = getAllRoom();
+  console.log(roomsName);
+  roomsName.forEach(el => {
+    let option = document.createElement('option');
+    option.value = el;
+    option.innerText = el;
+    $('#room_select').get(0).appendChild(option);
+  });
+
+  let peoplesName = getAllPeople();
+  peoplesName.forEach(el => {
+    let option = document.createElement('option');
+    option.value = el;
+    option.innerText = el;
+    $('#people_select').get(0).appendChild(option);
+  });
+};
+
+window.onload = () => {
+  initDropdown();
+
   $('#group_select option')[0].selected = true;
   selectedGroup = $('#group_select option')[0].value;
+  selectedPeople = selectedRoom = '';
   $('#group_select').change(_ => {
-    console.log('here');
     $('#group_select option').each((i, el) => {
       if (el.selected) {
         selectedGroup = el.value;
+      }
+    });
+    rewriteDOMForView(getInitState());
+  });
+
+  $('#room_select').change(_ => {
+    selectedGroup = selectedPeople = '';
+    $('#room_select option').each((i, el) => {
+      if (el.selected) {
+        selectedRoom = el.value;
+      }
+    });
+    rewriteDOMForView(getInitState());
+  });
+
+  $('#people_select').change(_ => {
+    selectedGroup = selectedRoom = '';
+    $('#people_select option').each((i, el) => {
+      if (el.selected) {
+        selectedPeople = el.value;
       }
     });
     rewriteDOMForView(getInitState());
@@ -129,29 +212,46 @@ const rewriteDOMForEdit = _ => {
 
   $('.table__row-wrapper .table__cell').each((i, el) => {
     let contentTop = '',
-      contentBottom = '';
-    if (el.children.length > 1) {
-      contentTop = el.children[0].innerText;
-      contentBottom = el.children[1].innerText;
-      el.innerHTML = '';
-      let inputTop = document.createElement('input');
-      inputTop.className = 'table__input';
-      inputTop.value = contentTop;
-      el.appendChild(inputTop);
-      let inputBottom = document.createElement('input');
-      inputBottom.className = 'table__input';
-      inputBottom.value = contentBottom;
-      el.appendChild(inputBottom);
+      contentBottom = '',
+      roomTop = '',
+      roomBottom = '',
+      peopleTop = '',
+      peopleBottom = '';
+    if (el.children.length === 2) {
+      console.log('im in');
+      console.log(el.children[0]);
+      console.log(el.children[0].children[0]);
+
+      contentTop = el.children[0].children[0].innerText;
+      roomTop = el.children[0].children[1].innerText;
+      peopleTop = el.children[0].children[2].innerText;
+      contentBottom = el.children[1].children[0].innerText;
+      roomBottom = el.children[1].children[1].innerText;
+      peopleBottom = el.children[1].children[2].innerText;
+      let res = createNodeCell({
+        context: contentTop,
+        room: roomTop,
+        people: peopleTop
+      }, 'input') + createNodeCell({
+        context: contentBottom,
+        room: roomBottom,
+        people: peopleBottom
+      }, 'input');
+      el.innerHTML = res;
     } else {
-      contentTop = contentBottom = el.innerText;
-      let input = document.createElement('input');
-      input.className = 'table__input';
-      input.value = contentTop;
-      el.innerHTML = '';
-      el.appendChild(input);
-      let inputBottom = document.createElement('input');
-      inputBottom.className = 'table__input';
-      el.appendChild(inputBottom);
+      contentTop = contentBottom = el.children[0].innerText;
+      roomTop = roomBottom = el.children[1].innerText;
+      peopleTop = peopleBottom = el.children[2].innerText;
+      let res = createNodeCell({
+        context: contentTop,
+        room: roomTop,
+        people: peopleTop
+      }, 'input') + createNodeCell({
+        context: contentTop,
+        room: roomTop,
+        people: peopleTop
+      }, 'input');
+      el.innerHTML = res;
     }
   });
 };
@@ -162,23 +262,31 @@ const rewriteDOMForView = parsedData => {
   if (!data[0][0].hasOwnProperty('top')) {
     return;
   }
-  console.log(data);
   $('.table__row-wrapper').each((i, cell) => {
     let row = Array.from(cell.children);
     row.forEach((el, j) => {
-      if (data[i][j].top === data[i][j].bottom) {
-        el.innerHTML = data[i][j].top;
+      if (data[i][j].top.context === data[i][j].bottom.context
+        && data[i][j].top.room === data[i][j].bottom.room
+        && data[i][j].top.people === data[i][j].bottom.people) {
+        el.innerHTML = createNodeCell(data[i][j].top, 'div');
       } else {
         el.innerHTML = '';
         let blockTop = document.createElement('div');
         blockTop.className = 'table__cell--top';
-        blockTop.innerText = data[i][j].top;
+        blockTop.innerHTML = createNodeCell(data[i][j].top, 'div');
         el.appendChild(blockTop);
         let blockBottom = document.createElement('div');
         blockBottom.className = 'table__cell--bottom';
-        blockBottom.innerText = data[i][j].bottom;
+        blockBottom.innerHTML = createNodeCell(data[i][j].bottom, 'div');
         el.appendChild(blockBottom);
       }
     });
   });
+};
+
+const createNodeCell = (data, tag) => {
+  let res = `<${tag} value='${data.context}'>${tag !== 'input' ? data.context : ''}</${tag}>
+             <${tag} value='${data.room}'>${tag !== 'input' ? data.room : ''}</${tag}>
+             <${tag} value='${data.people}'>${tag !== 'input' ? data.people : ''}</${tag}>`;
+  return res;
 };
